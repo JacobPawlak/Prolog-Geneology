b681ded6c6d82167d00f78ef2ef9071583d04e9d
JacobPawlak <jacob.pawlak@uky.edu>
2017-10-31 13:37:42 -0400

    added the children

--- family.pl
+++ family.pl
@@ -4,16 +4,16 @@ age(jacob_Pawlak, 21)
 age(matthew_Pawlak, 16)
 age(luke_Pawlak, 10)
 age(kristen_Pawlak, 50)
-age(ralph_Pawlak, 55)
+age(ralph_Pawlak, 55) 
 
 %the Matasichs
 
-age(dolice_Matasich, 78)
+age(dolice_Matasich, 78)  
 age(josef_Matasich, 82)
 
 %the OConnors
 
-age(pamela_OConnor, 65)
+age(pamela_OConnor, 65) 
 age(bill_OConnor, 68)
 age(will_OConnor, 34)
 age(kelly_OConnor, 36)
@@ -28,7 +28,7 @@ age(andrea_Pawlak, 32)
 age(terry_Pawlak, 58)
 age(steve_Shwartz, 60)
 age(amy_Dow, 30)
-age(Rebecca_Miller, 31)
+age(rebecca_Miller, 31)
 
 age(michelle_Pawlak, 52)
 age(jim_Pawlak, 57)
@@ -44,9 +44,39 @@ age(willem_Pawlak, 95)
 
 
 
-%children of Marge and William: Ralph, Jim, Rick, Terry, Pam, Juli
-child()
+%children of Marge and Willem: Ralph, Jim, Rick, Terry, Pam, Juli
 
+child(marge_Pawlak, ralph_Pawlak)
+child(marge_Pawlak, jim_Pawlak)
+child(marge_Pawlak, rick_Pawlak)
+child(marge_Pawlak, terry_Pawlak)
+child(marge_Pawlak, pamela_OConnor)
+child(marge_Pawlak, juli_Pawlak)
+
+child(willem_Pawlak, ralph_Pawlak)
+child(willem_Pawlak, jim_Pawlak)
+child(willem_Pawlak, rick_Pawlak)
+child(willem_Pawlak, terry_Pawlak)
+child(willem_Pawlak, pamela_OConnor)
+child(willem_Pawlak, juli_Pawlak)
+
+%children of Dolice and Josef: Kristen
+
+%children of Ralph and Kristen: Jacob, Matthew, Luke
+
+%children of Pamela and Bill: Will, Kelly
+
+%children of Sherri and Rick: Ricky, Andrea
+
+%children of Terry and Steve: Amy, Rebecca
+
+%children of Juli: Nicole, Jon
+
+%children of Jim and Michelle: Nik, Tyler
+
+
+
+%The rest of these need to be written implicitly
 
 parent()
 

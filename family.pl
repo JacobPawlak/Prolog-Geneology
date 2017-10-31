bca0993778111ca23fc15404eed727868fd5159f
Jacob Pawlak <jcpa229@cs.uky.edu>
2017-10-31 13:00:14 -0400

    added the ages for each of the family members

---
+++ family.pl
@@ -0,0 +1,63 @@
+%my Pawlaks
+
+age(jacob_Pawlak, 21)
+age(matthew_Pawlak, 16)
+age(luke_Pawlak, 10)
+age(kristen_Pawlak, 50)
+age(ralph_Pawlak, 55)
+
+%the Matasichs
+
+age(dolice_Matasich, 78)
+age(josef_Matasich, 82)
+
+%the OConnors
+
+age(pamela_OConnor, 65)
+age(bill_OConnor, 68)
+age(will_OConnor, 34)
+age(kelly_OConnor, 36)
+
+%the other Pawlaks
+
+age(sherri_Pawlak, 58)
+age(rick_Pawlak, 59)
+age(ricky_Pawlak, 33)
+age(andrea_Pawlak, 32)
+
+age(terry_Pawlak, 58)
+age(steve_Shwartz, 60)
+age(amy_Dow, 30)
+age(Rebecca_Miller, 31)
+
+age(michelle_Pawlak, 52)
+age(jim_Pawlak, 57)
+age(nik_Pawlak, 33)
+age(tyler_Pawlak, 34)
+
+age(juli_Pawlak, 52)
+age(nicole_McFarland, 31)
+age(jon_Sylvester, 30)
+
+age(marge_Pawlak, 91)
+age(willem_Pawlak, 95)
+
+
+
+%children of Marge and William: Ralph, Jim, Rick, Terry, Pam, Juli
+child()
+
+
+parent()
+
+
+sibling()
+
+
+nth-cousin()
+
+
+nth-cousin-k-times-removed()
+
+
+grandparent()
